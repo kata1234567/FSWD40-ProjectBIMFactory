0b42b8a8b89e29fb2779db091026814273f00d7e
Kathrin <kathrin@kathrins-air.codefactoryvienna.local>
2018-07-25 11:48:34 +0200

    index update

--- index.php
+++ index.php
@@ -83,29 +83,7 @@
         <!-- welcome -->
         <div class="bloc bloc-fill-screen b-parallax bloc-bg-texture texture-darken bgc-onyx bg-Creativity-207 d-bloc " id="welcome">
             <div class="container fill-bloc-top-edge sticky-nav">
-                <nav class="navbar row">
-                    <div class="logo navbar-header"><a class="navbar-brand creativity-logo" href="index.html"><span class="border"></span>
-            <p>CIA&nbsp;&nbsp;&nbsp;</p></a>
-                        <button id="nav-toggle" type="button" class="ui-navbar-toggle navbar-toggle" data-toggle="collapse" data-target=".navbar-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-                    </div>
-                    <div class="collapse navbar-collapse navbar-1">
-                        <ul class="site-navigation nav navbar-nav pull-right">
-                            <li>
-                                <div class="dropdown btn-dropdown"><a href="#" class="dropdown-toggle creativity-navigation creativity-folder" data-toggle="dropdown" aria-expanded="true">Our Courses<span class="caret"></span></a>
-                                    <ul class="dropdown-menu creativity-dropdown-box" role="menu">
-                                        <li><a href="design.html" class="a-btn a-block creativity-navigation">Backend</a></li>
-                                        <li><a href="marketing.html" class="a-btn a-block creativity-navigation">Frontend</a></li>
-                                        <li><a href="branding.html" class="a-btn a-block creativity-navigation">New Technologies</a></li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li><a href="about.html" class="creativity-navigation">About</a></li>
-                            <li><a href="team.html" class="creativity-navigation">Team</a></li>
-                            <li><a href="contact.html" class="creativity-navigation">Contact</a></li>
-                            <li><a class="btn  btn-d  btn-sm creativity-quote" href="contact.html" id="pl-undefined" target="_blank">Newsletter</a></li>
-                        </ul>
-                    </div>
-                </nav>
+                <?php include 'nav.php' ;?>
             </div>
             <div class="container">
                 <div class="row">
@@ -314,63 +292,7 @@
         </div>
         <!-- Bloc Group END -->
         <!-- Footer - counts -->
-        <div class="bloc b-parallax tc-white bloc-bg-texture texture-darken-strong bg-Creativity-201 bgc-white" id="counts">
-            <div class="container bloc-lg">
-                <div class="row">
-                    <div class="col-sm-3">
-                        <h2 class="mg-md text-center  fadeInUp tc-white creativity-yellow animated"><strong>28,288</strong></h2>
-                        <h4 class="text-center fadeInUp animated">Lines of code</h4></div>
-                    <div class="col-sm-3">
-                        <h2 class="mg-md text-center  fadeInUp animDelay02 tc-white creativity-yellow animated"><strong>152</strong></h2>
-                        <h4 class="text-center fadeInUp animDelay02 animated">Finished projects</h4></div>
-                    <div class="col-sm-3">
-                        <h2 class="mg-md text-center  fadeInUp animDelay04 tc-white creativity-yellow animated"><strong>1,023</strong></h2>
-                        <h4 class="text-center fadeInUp animDelay04 animated">Cups of coffee</h4></div>
-                    <div class="col-sm-3">
-                        <h2 class="mg-md text-center  fadeInUp animDelay06 tc-white creativity-yellow animated"><strong>Countless</strong></h2>
-                        <h4 class="text-center fadeInUp animDelay06 animated">Sleepless nights</h4></div>
-                </div>
-            </div>
-        </div>
-        <!-- Footer - counts END -->
-
-        
-        <!-- Footer - bloc-46 -->
-        <div class="bloc bgc-onyx d-bloc" id="bloc-46">
-            <div class="container bloc-md">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="row creativity-center">
-                            <div class="col-sm-12"><a href="http://facebook.com/gezalovef" class="btn btn-d btn-lg creativity-social-button" target="_blank"><span class="special-tag-for-editing-text-with-html"></span><span class="fa fa-facebook-official creativity-social-icon"></span></a><a href="http://instagram.com/eldar.gezalov" class="btn btn-d btn-lg creativity-social-button" target="_blank"><span class="fa fa-instagram creativity-social-icon"></span></a><a href="http://twitter.com/eldargezalov" class="btn btn-d btn-lg creativity-social-button" target="_blank"><span class="fa fa-twitter-square creativity-social-icon"></span></a><a href="http://blocstemplates.com" class="btn btn-d btn-lg creativity-social-button" target="_blank"><span class="fa fa-google-plus-square creativity-social-icon"></span></a></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Footer - bloc-46 END -->
-        <!-- Footer - footer -->
-        <div class="bloc tc-white bgc-onyx-2 d-bloc" id="footer">
-            <div class="container bloc-md">
-                <div class="row voffset">
-                    <div class="col-sm-3">
-                        <h1 class="mg-md creativity-logo">CIA<br></h1></div>
-                    <div class="col-sm-3">
-                        <h4 class="mg-md tc-white creativity-yellow"><strong>Contact</strong></h4>
-                        <p>Tel. +123 1234 1234
-                            <br> cia@codeinstituteaustria.at</p>
-                    </div>
-                    <div class="col-sm-3">
-                        <h4 class="mg-md tc-white creativity-yellow"><strong>Location</strong></h4>
-                        <p>28 Blocs Street, Los Angeles,
-                            <br>CA, 12345, USA</p>
-                    </div>
-                    <div class="col-sm-3">
-                        <h4 class="mg-md tc-white creativity-yellow"><strong>About</strong></h4>
-                        <p>We are the leading coding institute located in Austria</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php include 'footer.php'; ?>
         <!-- Footer - footer END -->
     </div>
     <!-- Main container END -->
